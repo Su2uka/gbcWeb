028c7862f93e89f443c71e2e85f99ba5bcbbaa8b
Tt <3237289802@qq.com>
2024-10-30 22:16:09 +0800

    local v1.0 optimize loader

--- src/main/webapp/js/songLikeListLoader.js
+++ src/main/webapp/js/songLikeListLoader.js
@@ -41,148 +41,98 @@ $(function () {
 });
 
 function load(currentPage, search) {
-    //发送ajax请求
+    // 发送 AJAX 请求
     $.get("song/likePageQuery", {currentPage: currentPage, search: search}, function (pb) {
         currentPageNum = currentPage;
 
-        //1.分页工具条展示
-        var lis = "";
+        // 1. 分页工具条展示
+        const lis = [];
 
-        var firstPage = '<li onclick="javascript:like(); load(null,\'' + search + '\'); "><a href="javascript:void(0)">首页</a></li>';
+        // 首页和上一页
+        const preNum = Math.max(pb.currentPage - 1, 1);
+        lis.push(`
+            <li onclick="like(); load(null, '${search}');"><a href="#">首页</a></li>
+            <li onclick="like(); load(${preNum}, '${search}');">
+                <a href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                </a>
+            </li>
+        `);
 
-        //计算上一页的页码
-        var preNum = pb.currentPage - 1;
-        if (preNum <= 1) {
-            preNum = 1;
-        }
+        // 计算页码范围
+        let begin = Math.max(pb.currentPage - 2, 1);
+        let end = Math.min(pb.currentPage + 2, pb.totalPage);
 
-        var prepage = '<li onclick="javascript:like(); load(' + preNum + ',\'' + search + '\'); ">\n' +
-            '          <a href="javascript:void(0);" aria-label="Previous">\n' +
-            '            <span aria-hidden="true">&laquo;</span>\n' +
-            '          </a>\n' +
-            '        </li>';
-
-        lis += firstPage;
-        lis += prepage;
-
-        var begin;  //开始位置
-        var end;  //结束位置
-
-        //显示5个页码
-        if (pb.totalPage < 5) {
-            begin = 1;
-            end = pb.totalPage;
-        } else {
-
-            begin = pb.currentPage - 2;
-            end = pb.currentPage + 2;
-
-            //如果前面不够2个，后面补齐
-            if (begin < 1) {
-                begin = 1;
-                end = begin + 4;
-            }
-
-            //如果后面不足2个,前面补齐
-            if (end > pb.totalPage) {
-                end = pb.totalPage;
-                begin = end - 4;
-            }
-        }
-
-        for (var i = begin; i <= end; i++) {
-            var li;
-            //判断当前页码是否等于i
-            if (i == pb.currentPage) {
-                li = '<li class="active" onclick="javascript:like(); load(' + i + ',\'' + search + '\');"><a href="javascript:void(0);">' + i + '</a></li>';
-            } else {
-                li = '<li onclick="javascript:like(); load(' + i + ',\'' + search + '\'); "><a href="javascript:void(0);">' + i + '</a></li>';
-            }
-            lis += li;
-        }
-
-        //计算下一页的页码
-        var nextNum = pb.currentPage + 1;
-        if (nextNum >= pb.totalPage) {
-            nextNum = pb.totalPage;
-        }
-
-        var nextPage = '<li onClick="like(); javascript:load(' + nextNum + ',\'' + search + '\');">\n' +
-            '          <a href="javascript:void(0);" aria-label="Next">\n' +
-            '            <span aria-hidden="true">&raquo;</span>\n' +
-            '          </a>\n' +
-            '        </li>';
-        var lastPage = '<li onclick="javascript:like(); load(' + pb.totalPage + ',\'' + search + '\');"><a href="javascript:void(0);">末页</a></li>';
-
-        lis += nextPage;
-        lis += lastPage;
-
-        $("#pageNum").html(lis);
-
-
-
-
-        //2. 列表数据展示
-        var song_lis = "";
-
-        // 播放器当前播放歌曲的 id
-        var currentId = $(".audioPlayer").attr('id');
-        currentId = parseInt(currentId);  // 确保 currentId 是数字
-        var audio = $("#myAudio")[0];
-
-        // 遍历歌曲列表
-        for (var i = 0; i < pb.list.length; i++) {
-            var song = pb.list[i];
-            var li = '';  // 在循环的每次迭代中初始化 li，确保它不会保持上次循环的值
-
-            // 处理播放状态和喜欢状态
-            var isPlaying = (song.sid == currentId && isMusicPlaying(audio));
-            var isLiked = false;
-
-            // 查找该歌曲是否在 likeList 中
-            for (var j = 0; j < likeList.length; j++) {
-                var likeId = likeList[j].sid;
-                /*console.log(likeId);*/
-                if (song.sid == likeId) {
-                    isLiked = true;
-                    break;
+        if (pb.totalPage > 5) {
+            if (end - begin < 4) {
+                if (begin === 1) {
+                    end = begin + 4;
+                } else if (end === pb.totalPage) {
+                    begin = end - 4;
                 }
             }
-
-            // 构建 li 标签
-            li += '<li class="song-item" id="' + song.sid + '">\n' +
-                '    <img src="' + song.cover + '" alt="Song Cover" class="song-cover">\n' +
-                '    <div class="song-info">\n' +
-                '        <h5 class="song-title" title="' + song.title + '">' + song.title + '</h5>\n' +
-                '        <p class="song-artist">' + song.artist + '</p>\n' +
-                '        <p class="song-album">' + song.album + '</p>\n' +
-                '    </div>\n' +
-                '    <div class="song-actions">\n' +
-                '        <span class="song-duration">' + song.duration + '</span>\n' +
-                '        <a href="#" class="icon-link download-button">\n' +
-                '            <span class="glyphicon glyphicon-download-alt download-icon"></span>\n' +
-                '        </a>\n';
-
-            // 添加播放按钮
-            li += '        <a href="#" class="icon-link play-button">\n' +
-                '            <span class="glyphicon glyphicon-' + (isPlaying ? 'pause' : 'play') + ' play-icon"></span>\n' +
-                '        </a>\n';
-
-            // 添加喜欢按钮
-            li += '        <a href="#" class="icon-link like-button">\n' +
-                '            <span class="glyphicon glyphicon-heart like-icon ' + (isLiked ? 'like-active' : '') + '"></span>\n' +
-                '        </a>\n';
-
-            // 添加音频元素
-            li += '        <audio src="' + song.filepath + '"></audio>\n' +
-                '    </div>\n' +
-                '</li>';
-
-            // 将 li 添加到 song_lis 中
-            song_lis += li;
+        } else {
+            begin = 1;
+            end = pb.totalPage;
         }
 
+        // 中间页码
+        for (let i = begin; i <= end; i++) {
+            lis.push(`
+                <li class="${i === pb.currentPage ? 'active' : ''}" onclick="like(); load(${i}, '${search}');">
+                    <a href="#">${i}</a>
+                </li>
+            `);
+        }
+
+        // 下一页和末页
+        const nextNum = Math.min(pb.currentPage + 1, pb.totalPage);
+        lis.push(`
+            <li onclick="like(); load(${nextNum}, '${search}');">
+                <a href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                </a>
+            </li>
+            <li onclick="like(); load(${pb.totalPage}, '${search}');"><a href="#">末页</a></li>
+        `);
+
+        $("#pageNum").html(lis.join(''));
+
+        // 2. 列表数据展示
+        const songItems = pb.list.map(song => {
+            // 处理播放状态和喜欢状态
+            const currentId = parseInt($(".audioPlayer").attr('id'));
+            const audio = $("#myAudio")[0];
+            const isPlaying = song.sid === currentId && isMusicPlaying(audio);
+            const isLiked = likeList.some(like => like.sid === song.sid);
+
+            return `
+                <li class="song-item" id="${song.sid}">
+                    <img src="${song.cover}" alt="Song Cover" class="song-cover">
+                    <div class="song-info">
+                        <h5 class="song-title" title="${song.title}">${song.title}</h5>
+                        <p class="song-artist">${song.artist}</p>
+                        <p class="song-album">${song.album}</p>
+                    </div>
+                    <div class="song-actions">
+                        <span class="song-duration">${song.duration}</span>
+                        <a href="${song.download || '#'}" class="icon-link download-button">
+                            <span class="glyphicon glyphicon-download-alt download-icon"></span>
+                        </a>
+                        <a href="#" class="icon-link play-button">
+                            <span class="glyphicon glyphicon-${isPlaying ? 'pause' : 'play'} play-icon"></span>
+                        </a>
+                        <a href="#" class="icon-link like-button">
+                            <span class="glyphicon glyphicon-heart like-icon ${isLiked ? 'like-active' : ''}"></span>
+                        </a>
+                        <audio src="${song.filepath}"></audio>
+                    </div>
+                </li>
+            `;
+        });
+
         // 更新歌曲列表
-        $("#song-list").html(song_lis);
-    })
+        $("#song-list").html(songItems.join(''));
+    });
 }
+
